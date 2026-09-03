eac1da08a772b3d0355b9d7d756447c15df44d4f
Papa Cissé <papac8443@gmail.com>
2026-09-03 11:35:12 +0000

    feat: définir le contrat du repository

---
+++ src/Repository/CopieExamenRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\CopieExamen;
+
+interface CopieExamenRepositoryInterface
+{
+    public function save(CopieExamen $copie): int;
+
+    /** @return CopieExamen[] */
+    public function findAll(): array;
+
+    public function findById(int $id): ?CopieExamen;
+}
