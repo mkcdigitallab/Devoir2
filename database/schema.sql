dbcbac403e01e98eb4f0c79d80fdc4ca80a302b8
Papa Cissé <papac8443@gmail.com>
2026-09-03 11:39:33 +0000

    refactor: séparer le schéma et les données de démonstration

--- database/schema.sql
+++ database/schema.sql
@@ -6,6 +6,3 @@ CREATE TABLE IF NOT EXISTS copie_examen (
     penalite_appliquee BOOLEAN NOT NULL DEFAULT FALSE,
     date_limite TIMESTAMP NOT NULL
 );
-
-INSERT INTO copie_examen (date_depot, note_brute, note_finale, penalite_appliquee, date_limite)
-VALUES ('2024-01-15 10:30:00', 15.5, 15.5, FALSE, '2024-01-20 23:59:59');
